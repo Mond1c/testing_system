d45af4136582c4081a018532603d4de52172355f
Kornilovich Mikhail <kornilovichmihail@gmail.com>
2024-04-16 13:14:28 +0300

    added structures for contest

---
+++ internal/contest.go
@@ -0,0 +1,47 @@
+package internal
+
+import "time"
+
+type ProblemInfo = string
+
+type RunInfo struct {
+	result TestingResult
+	time   time.Duration
+}
+
+func NewRunInfo(result TestingResult, t time.Duration) *RunInfo {
+	return &RunInfo{
+		result: result,
+		time:   t,
+	}
+}
+
+type ContestantInfo struct {
+	Name    string                    `json:"name"`
+	Points  int                       `json:"points"`
+	Penalty int                       `json:"penalty"`
+	Runs    map[ProblemInfo][]RunInfo `json:"runs"`
+}
+
+func NewContestantInfo(name string) *ContestantInfo {
+	return &ContestantInfo{
+		Name:    name,
+		Points:  0,
+		Penalty: 0,
+		Runs:    make(map[ProblemInfo][]RunInfo),
+	}
+}
+
+type ContestInfo struct {
+	Problems    []ProblemInfo    `json:"problems"`
+	Contestants []ContestantInfo `json:"contestants"`
+	StartTime   time.Time        `json:"start_time"`
+}
+
+func NewContestInfo(problems []ProblemInfo, contestants []ContestantInfo, startTime time.Time) *ContestInfo {
+	return &ContestInfo{
+		Problems:    problems,
+		Contestants: contestants,
+		StartTime:   startTime,
+	}
+}
